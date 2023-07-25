e54ce0da7cc8cced86b1055770bcbdb7a8c40888
AVES <binhome219@gmail.com>
2023-07-25 18:02:24 +0800

    重新命名購物車檔名

--- shoping.php
+++ shoping.php
@@ -34,10 +34,12 @@
 <body>
     <?php
     session_start();
-    /*$_SESSION["tel"] = $_POST["tel"];
-    $userFileName = "user/" . $_POST["tel"] . ".txt";
-    $userFile = fopen($userFileName, 'a');
-    fclose($userFile);*/
+    $_SESSION["tel"] = $_POST["tel"];
+    if(isset($_SESSION["tel"])){
+        $userFileName = "user/" . $_POST["tel"] . ".txt";
+        $userFile = fopen($userFileName, 'a');
+        fclose($userFile);
+    }
     //set user
     
     $file = file("product.txt");
