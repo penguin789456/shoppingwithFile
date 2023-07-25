b0e01f664c7130165715fc8a421e0df18860fd4f
AVES <binhome219@gmail.com>
2023-07-25 23:01:14 +0800

    重設資料傳遞方式

--- shoping.php
+++ shoping.php
@@ -52,6 +52,7 @@
     foreach ($file as $x) {
         array_push($product, explode(",", $x));
     }
+    $_SESSION["pd"]=$product;
     //set product array
     ?>
     <div class="container">
@@ -65,16 +66,16 @@
                         <table>
                         <tr>
                         <td rowspan='3'><img src='pics\\".$det[3]."'></td>
-                        <td name='ID'>ID：".$det[0]."</td>
+                        <td >ID：".$det[0]."</td>
                         </tr>
                         <tr>
-                            <td name='NAME'>名稱：".$det[1]."</td>
+                            <td>名稱：".$det[1]."</td>
                         </tr>
                         <tr>
-                            <td name='PRICE'>價格".$det[2]."</td>
+                            <td>價格".$det[2]."</td>
                         </tr>
                         <tr>
-                            <td colspan='2' style='text-align:center'><input type='submit' value='加入購物車'></td>
+                            <td colspan='2' style='text-align:center'><a href='shoppingObject.php?id=".$det[0]."'>加入購物車</a></td>
                         </tr>
                         </table>
                         ";
