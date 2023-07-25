ad4b8637d2a6c6663fade85b798d4da14a84cc78
AVES <binhome219@gmail.com>
2023-07-26 03:43:18 +0800

    將紀錄使用者存檔時間完成

--- finCart.php
+++ finCart.php
@@ -8,6 +8,17 @@
 <body>
     <?php 
         session_start();
+        $total = $_SESSION["total"];
+        $tel = $_SESSION["tel"];
+
+
+        $fileN="bill.txt";
+        $fp = fopen($fileN,"a");
+        date_default_timezone_set('Asia/Taipei'); //設定時間為台北時區
+        $date = date('YmdHis');
+        $content = "$date,$tel,$total";
+        fwrite($fp,"$content");
+        fclose($fp);
         echo "<h1>感謝購買總金額為：".$_SESSION["total"]."</h1>";
     ?>
     <h2><a href="shoping.php">返回商品目錄</a></h2>
